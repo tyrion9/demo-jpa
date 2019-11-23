4974e47ae7399696bd151d8736a1eae60d337031
hoaipn <hoaipn@gmail.com>
2019-11-23 23:18:25 +0700

    Thêm ghi chú

--- src/main/java/demo/demojpa/domain/DemoInRepository.java
+++ src/main/java/demo/demojpa/domain/DemoInRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+/**
+ * Demo sử dụng query in ở tham số
+ */
 public interface DemoInRepository extends JpaRepository<Customer, Long> {
 
     @Query ("select c from Customer c where c.address in (:address)")
